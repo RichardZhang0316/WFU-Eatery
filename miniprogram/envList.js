71aad13c466e2bfd4e23d8454dc141e1da98df93
unknown <unknown@example.com>
2021-10-16 00:21:28 -0400

    Initial Commit

---
+++ miniprogram/envList.js
@@ -0,0 +1,6 @@
+const envList = [{"envId":"cloud1-9g617xow636319d2","alias":"cloud1"}]
+const isMac = true
+module.exports = {
+    envList,
+    isMac
+}
---
+++ uploadCloudFunction.sh
@@ -0,0 +1,1 @@
+"/Applications/wechatwebdevtools.app/Contents/MacOS/cli" cloud functions deploy --e cloud1-9g617xow636319d2 --n quickstartFunctions --r --project "/Users/zhangkaixin/WeChatProjects/WFUeatery" --report_start --report
