20e9560d52d1c0366ac973d2d67f1c2fe80f803d
richard <zhank20@wfu.edu>
2021-11-09 14:54:26 -0500

    2

--- miniprogram/pages/IncomingMenu/IncomingMenu.js
+++ miniprogram/pages/IncomingMenu/IncomingMenu.js
@@ -1,4 +1,4 @@
-// pages/IncomingMenu/IncomingMenu.js
+var util = require('../../utils/util.js');
 Page({
 
     /**
@@ -12,7 +12,29 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+      //获取当前日期
+      var time = util.formatDate(new Date());
+      var day1 = util.getTimeLastWeek1(new Date());//1天后时间
+      var day2 = util.getTimeLastWeek2(new Date());
+      var day3 = util.getTimeLastWeek3(new Date());
+      var day4 = util.getTimeLastWeek4(new Date());
+      var day5 = util.getTimeLastWeek5(new Date());
+      var day6 = util.getTimeLastWeek6(new Date());
+      var week1=util.getWeekByDate1(new Date());
+      var week2=util.getWeekByDate2(new Date());
+      var week3=util.getWeekByDate3(new Date());
+      var week4=util.getWeekByDate4(new Date());
+      var week5=util.getWeekByDate5(new Date());
+      var week6=util.getWeekByDate6(new Date());
+      this.setData({
+        time:'Today\n'+time,
+        day1:week1+'\n'+day1,
+        day2:week2+'\n'+day2,
+        day3:week3+' \n'+day3,
+        day4:week4+' \n'+day4,
+        day5:week5+'\n'+day5,
+        day6:week6+'\n'+day6,
+      });
     },
 
     /**
@@ -76,13 +98,13 @@ Page({
         }
     },
 })
-function getDay(num) {
-    var today = new Date();
-    var nowTime = today.getTime();
-    var ms = 24 * 3600 * 1000 * num;
-    today.setTime(parseInt(nowTime + ms));
+// function getDay(num) {
+//     var today = new Date();
+//     var nowTime = today.getTime();
+//     var ms = 24 * 3600 * 1000 * num;
+//     today.setTime(parseInt(nowTime + ms));
   
-    var oMoth = (today.getMonth() + 1).toString();
-    var oDay = today.getDate().toString();
-    return oMoth + '月' + oDay + '日';
-  }
+//     // var oMoth = (today.getMonth() + 1).toString();
+//     var oDay = today.getDate().toString();
+//     return oDay ;
+//   }
---
+++ miniprogram/utils/util.js
@@ -0,0 +1,223 @@
+const formatTime = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+
+  return `${[year, month, day].map(formatNumber).join('/')} ${[hour, minute, second].map(formatNumber).join(':')}`
+}
+
+const formatDate = date => {
+  const day = date.getDate()
+
+  return `${[day].map(formatNumber)}`
+}
+const getWeekByDate1 = dates => {
+  let show_day = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat');
+  let date = new Date(dates);
+  date.setDate(date.getDate());
+  let day = date.getDay();
+  return show_day[(day+1)%7];
+}
+const getWeekByDate2 = dates => {
+  let show_day = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat');
+  let date = new Date(dates);
+  date.setDate(date.getDate());
+  let day = date.getDay();
+  return show_day[(day+2)%7];
+}
+const getWeekByDate3 = dates => {
+  let show_day = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat');
+  let date = new Date(dates);
+  date.setDate(date.getDate());
+  let day = date.getDay();
+  return show_day[(day+3)%7];
+}
+const getWeekByDate4 = dates => {
+  let show_day = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat');
+  let date = new Date(dates);
+  date.setDate(date.getDate());
+  let day = date.getDay();
+  return show_day[(day+4)%7];
+}
+const getWeekByDate5 = dates => {
+  let show_day = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat');
+  let date = new Date(dates);
+  date.setDate(date.getDate());
+  let day = date.getDay();
+  return show_day[(day+5)%7];
+}
+const getWeekByDate6 = dates => {
+  let show_day = new Array('Sun', 'Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat');
+  let date = new Date(dates);
+  date.setDate(date.getDate());
+  let day = date.getDay();
+  return show_day[(day+6)%7];
+}
+
+const getTimeLastWeek1 = last => {
+  const year = last.getFullYear()
+  const day = last.getDate()
+  const ti = day + 1
+  // const dayOfWeek = last.getDay() //今天本周的第几天  
+  // 判断是否月初
+  if (ti <= 0) {
+    const month = last.getMonth() + 1 - 1
+    const d = new Date(year, month, 0)
+    const dayBig = d.getDate() //获取当月的所有天数
+    const ti1 = dayBig + ti
+    return [ti1].map(formatNumber)
+  } else {
+    const month = last.getMonth() + 1
+    return [ti].map(formatNumber)
+  }
+  // return [year, month, day].map(formatNumber).join('-')
+
+}
+const getTimeLastWeek2 = last => {
+  const year = last.getFullYear()
+  const day = last.getDate()
+  const ti = day + 2
+  // const dayOfWeek = last.getDay() //今天本周的第几天  
+  // 判断是否月初
+  if (ti <= 0) {
+    const month = last.getMonth() + 1 - 1
+    const d = new Date(year, month, 0)
+    const dayBig = d.getDate() //获取当月的所有天数
+    const ti1 = dayBig + ti
+    return [ti1].map(formatNumber)
+  } else {
+    const month = last.getMonth() + 1
+    return [ti].map(formatNumber)
+  }
+  // return [year, month, day].map(formatNumber).join('-')
+
+}
+
+const getTimeLastWeek3 = last => {
+  const year = last.getFullYear()
+  const day = last.getDate()
+  const ti = day + 3
+  // const dayOfWeek = last.getDay() //今天本周的第几天  
+  // 判断是否月初
+  if (ti <= 0) {
+    const month = last.getMonth() + 1 - 1
+    const d = new Date(year, month, 0)
+    const dayBig = d.getDate() //获取当月的所有天数
+    const ti1 = dayBig + ti
+    return [ti1].map(formatNumber)
+  } else {
+    const month = last.getMonth() + 1
+    return [ti].map(formatNumber)
+  }
+  // return [year, month, day].map(formatNumber).join('-')
+
+}
+
+const getTimeLastWeek4 = last => {
+  const year = last.getFullYear()
+  const day = last.getDate()
+  const ti = day + 4
+  // const dayOfWeek = last.getDay() //今天本周的第几天  
+  // 判断是否月初
+  if (ti <= 0) {
+    const month = last.getMonth() + 1 - 1
+    const d = new Date(year, month, 0)
+    const dayBig = d.getDate() //获取当月的所有天数
+    const ti1 = dayBig + ti
+    return [ti1].map(formatNumber)
+  } else {
+    const month = last.getMonth() + 1
+    return [ti].map(formatNumber)
+  }
+  // return [year, month, day].map(formatNumber).join('-')
+
+}
+
+const getTimeLastWeek5 = last => {
+  const year = last.getFullYear()
+  const day = last.getDate()
+  const ti = day + 5
+  // const dayOfWeek = last.getDay() //今天本周的第几天  
+  // 判断是否月初
+  if (ti <= 0) {
+    const month = last.getMonth() + 1 - 1
+    const d = new Date(year, month, 0)
+    const dayBig = d.getDate() //获取当月的所有天数
+    const ti1 = dayBig + ti
+    return [ti1].map(formatNumber)
+  } else {
+    const month = last.getMonth() + 1
+    return [ti].map(formatNumber)
+  }
+  // return [year, month, day].map(formatNumber).join('-')
+
+}
+
+const getTimeLastWeek6 = last => {
+  const year = last.getFullYear()
+  const day = last.getDate()
+  const ti = day + 6
+  // const dayOfWeek = last.getDay() //今天本周的第几天  
+  // 判断是否月初
+  if (ti <= 0) {
+    const month = last.getMonth() + 1 - 1
+    const d = new Date(year, month, 0)
+    const dayBig = d.getDate() //获取当月的所有天数
+    const ti1 = dayBig + ti
+    return [ti1].map(formatNumber)
+  } else {
+    const month = last.getMonth() + 1
+    return [ti].map(formatNumber)
+  }
+  // return [year, month, day].map(formatNumber).join('-')
+
+}
+
+const formatNumber = n => {
+  n = n.toString()
+  return n[1] ? n : `0${n}`
+}
+
+module.exports = {
+  formatTime
+}
+
+// util.js
+const formatTimeNEW = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  const hour = date.getHours()
+  const minute = date.getMinutes()
+  const second = date.getSeconds()
+ 
+ return [year, month, day].map(formatNumberNEW).join('/') + ' ' + [hour, minute].map(formatNumberNEW).join(':')   //返回年月日，时分秒
+}
+
+const formatNumberNEW = n => {
+ n = n.toString()
+ return n[1] ? n : '0' + n
+}
+
+module.exports = {
+ formatTimeNEW: formatTimeNEW,
+ formatDate:formatDate,
+ getTimeLastWeek1: getTimeLastWeek1,
+ getTimeLastWeek2: getTimeLastWeek2,
+ getTimeLastWeek3: getTimeLastWeek3,
+ getTimeLastWeek4: getTimeLastWeek4,
+ getTimeLastWeek5: getTimeLastWeek5,
+ getTimeLastWeek6: getTimeLastWeek6,
+ getWeekByDate1:getWeekByDate1,
+ getWeekByDate2:getWeekByDate2,
+ getWeekByDate3:getWeekByDate3,
+ getWeekByDate4:getWeekByDate4,
+ getWeekByDate5:getWeekByDate5,
+ getWeekByDate6:getWeekByDate6,
+}
+// module.exports.formatTimeNEW = formatTimeNEW
+// exports.formatTimeNEW = formatTimeNEW
+
