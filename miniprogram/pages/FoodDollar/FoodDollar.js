3f5baff7c1f742ea1a1691a72795de5283da630b
richard <zhank20@wfu.edu>
2021-11-24 19:13:14 -0500

    第三页更新

--- miniprogram/pages/FoodDollar/FoodDollar.js
+++ miniprogram/pages/FoodDollar/FoodDollar.js
@@ -19,6 +19,22 @@ Page({
         desc: '描述信息',
       },
     ],
+    contents:'这是可以复制的文字,粘贴后即可看到效果',
+  },
+  copyText: function (e) {
+    console.log(e)
+    wx.setClipboardData({
+      data: e.currentTarget.dataset.text,
+      success: function (res) {
+        wx.getClipboardData({
+          success: function (res) {
+            wx.showToast({
+              title: '复制成功'
+            })
+          }
+        })
+      }
+    })
   },
 
   /**
