e19ab9f7366b4cecfba6eb4b37a94d2e6adc6153
MichaelXi3 <92639450+MichaelXi3@users.noreply.github.com>
2022-04-13 14:03:39 -0400

    some basic commentList

--- miniprogram/pages/Favorites/Favorites.js
+++ miniprogram/pages/Favorites/Favorites.js
@@ -1,66 +1,24 @@
-// pages/Favorites/Favorites.js
 Page({
 
-    /**
-     * 页面的初始数据
-     */
     data: {
-
+        name: "Deacon"
     },
 
-    /**
-     * 生命周期函数--监听页面加载
-     */
     onLoad: function (options) {
 
     },
 
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
+    getUserProfile: function (e) {
+        var that = this
+        wx.getUserProfile({
+          desc: '业务需要',
+          success: res => {
+            console.log(res)
+            this.setData({
+                'name': res.userInfo.nickName
+            })
+          }
+        })       
+      }
 
-    },
-
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
 })
--- miniprogram/pages/HomePage/HomePage.js
+++ miniprogram/pages/HomePage/HomePage.js
@@ -3,12 +3,13 @@ const app = getApp()
 // import util from '../../utils/util.js';
 var util = require('../../utils/util.js');
 var myDate = new Date();
-Page({
 
-    /**
-     * 页面的初始数据
-     */
+Page({
     data: { 
+        // 用户授权所需Data
+        // userInfo: {},
+        // hasUserInfo: false,
+
         chosenPayment:"",
         eateries: [{
                 name: "Zick's",
@@ -457,10 +458,13 @@ Page({
             chosenPayment
         })
     },
-    /**
-     * 生命周期函数--监听页面加载
-     */
+
+    // 生命周期函数--监听页面加载
     onLoad: function (options) {
+        // 以下是用户授权登录的code
+        var openid = wx.getStorageInfo('openId');
+
+        // 以下是HomePage主要内容js
         var currentTime=new Date().getHours();
         var sentences="美食与温暖，我们都有";
         if(currentTime>=0&&currentTime<=5){
@@ -536,59 +540,7 @@ Page({
         this.setData({
             eateries: eateries
         })
-    },
-
-    /**
-     * 调动时间
-     */
-    timesetting: function () {},
-
-    /**
-     * 生命周期函数--监听页面初次渲染完成
-     */
-    onReady: function () {
 
     },
 
-    /**
-     * 生命周期函数--监听页面显示
-     */
-    onShow: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面隐藏
-     */
-    onHide: function () {
-
-    },
-
-    /**
-     * 生命周期函数--监听页面卸载
-     */
-    onUnload: function () {
-
-    },
-
-    /**
-     * 页面相关事件处理函数--监听用户下拉动作
-     */
-    onPullDownRefresh: function () {
-
-    },
-
-    /**
-     * 页面上拉触底事件的处理函数
-     */
-    onReachBottom: function () {
-
-    },
-
-    /**
-     * 用户点击右上角分享
-     */
-    onShareAppMessage: function () {
-
-    }
 })
